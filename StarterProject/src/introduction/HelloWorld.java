dc602f94f82017ed739a7013d476f1d7da9082f1
NatalijaGajic <nat.gaj98@gmail.com>
2023-02-28 19:35:31 +0100

    Introduction to variables

--- StarterProject/src/introduction/HelloWorld.java
+++ StarterProject/src/introduction/HelloWorld.java
@@ -5,6 +5,18 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		
+		int a = 5;
+		int b = 6;
+		
+		if(a > b) {
+			System.out.println("a je vece od b");
+		}else if(a < b) {
+			System.out.println("a je manje od b");
+
+		}else {
+			System.out.println("a i b su isti");
+		}
 	}
 
 }
